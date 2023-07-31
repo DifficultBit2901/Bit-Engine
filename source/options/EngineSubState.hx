ddddc0267a0ca61de51c342043e157e38c20c93d
Difficult_Bit2901 <dbhero2901@gmail.com>
2023-07-31 18:15:35 +0200

    removed some of the time bars

--- source/options/EngineSubState.hx
+++ source/options/EngineSubState.hx
@@ -51,7 +51,7 @@ class EngineSubState extends BaseOptionsMenu
 			'timeBarStyle',
 			'string',
 			'Difficult Engine',
-			['Difficult Engine', 'Psych Engine', 'Kade Engine Legacy', 'Leather Engine', 'Dave Engine', 'Dave Engine Alt', 'Opponent Color']);
+			['Difficult Engine', 'Psych Engine', 'Opponent Color']);
 		addOption(option);
 
 		#if MODS_ALLOWED
